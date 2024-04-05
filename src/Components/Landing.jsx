9519a5c41464b568c3bdfd4952b168b657e5c75c
ArunavaCoderEm <acecankill@gmail.com>
2024-04-06 01:42:27 +0530

    push

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -28,7 +28,7 @@ export default function Landing() {
   }
   return (
     <>
-    <div className='mobout h-auto rounded-md bg-slate-300 imgbut relative m-5 bg-[url("https://mir-s3-cdn-cf.behance.net/project_modules/1400/37e19c53319193.592ffe63763e0.gif")]'>
+    <div className='mobout h-auto rounded-md bg-slate-300 imgbut m-5 bg-[url("https://mir-s3-cdn-cf.behance.net/project_modules/1400/37e19c53319193.592ffe63763e0.gif")]'>
       {today.map((recipe) => {
             return(
               <div key={recipe.id} className='grid grid-cols-7 overflow-hidden py-5 mobhid'>
@@ -41,7 +41,7 @@ export default function Landing() {
                     <mark className='tit text-center font-semibold px-3 mb-4'>" {(recipe.title).toUpperCase()}"</mark>
                   </div>
                   <h3 className='fog bg-slate-200 font-semibold py-2 rounded-lg opacity-95 nextim mb-7 w-full text-center px-3 text-pink-600'>"Recipes are the stories that food tells, capturing flavors, traditions, and memories within every ingredient."</h3>
-                  <button className='tr focus:outline-none bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 text-center font-semibold text-sky-200 font-mono relative'>Get Recipe</button>
+                  <button className='tr focus:outline-none bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 rounded-lg text-sm px-5 py-3 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 text-center font-semibold text-sky-200 font-mono'>Get Recipe</button>
                 </div>
                 <div className="nnextim col-span-2">
                   <img src='./chef.png' className='transform -scale-x-100 rounded-md opacity-90 w-96 h-80 relative m-5'/>
